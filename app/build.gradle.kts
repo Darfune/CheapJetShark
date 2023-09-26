dca9706198b99251764f4387a1e8358826aae299
Darfune <harolep@gmail.com>
2023-09-26 20:29:34 +0300

    Coil setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Coil library for remote image loading
+    implementation("io.coil-kt:coil-compose:2.4.0")
 }
