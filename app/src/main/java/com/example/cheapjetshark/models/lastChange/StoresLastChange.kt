bdbecec362d767b74c55d5c505632a1924d82e79
Darfune <harolep@gmail.com>
2023-09-28 13:33:47 +0300

    models

---
+++ app/src/main/java/com/example/cheapjetshark/models/lastChange/StoresLastChange.kt
@@ -0,0 +1,24 @@
+package com.example.cheapjetshark.models.lastChange
+
+data class StoresLastChange(
+    val `1`: String,
+    val `2`: String,
+    val `3`: String,
+    val `7`: String,
+    val `8`: String,
+    val `11`: String,
+    val `13`: String,
+    val `15`: String,
+    val `21`: String,
+    val `23`: String,
+    val `24`: String,
+    val `25`: String,
+    val `27`: String,
+    val `28`: String,
+    val `29`: String,
+    val `30`: String,
+    val `31`: String,
+    val `33`: String,
+    val `34`: String,
+    val `35`: String
+)
