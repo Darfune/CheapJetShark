70507aeb0217d47ca6cddcf8b82a5ebbf53748d2
Darfune <harolep@gmail.com>
2023-09-28 21:06:04 +0300

    change Singleton to SingletonComponent

--- app/src/main/java/com/example/cheapjetshark/di/AppModule.kt
+++ app/src/main/java/com/example/cheapjetshark/di/AppModule.kt
@@ -5,12 +5,13 @@ import com.example.cheapjetshark.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(Singleton::class)
+@InstallIn(SingletonComponent::class)
 class AppModule {
 
     //Retrofit
