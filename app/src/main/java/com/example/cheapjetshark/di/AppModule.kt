8fa6d0ea001b6993dd12f52c11770ca38774524f
Darfune <harolep@gmail.com>
2023-09-28 16:35:16 +0300

    AppModule added with retrofit provider

--- app/src/main/java/com/example/cheapjetshark/di/AppModule.kt
+++ app/src/main/java/com/example/cheapjetshark/di/AppModule.kt
@@ -1,4 +1,25 @@
 package com.example.cheapjetshark.di
 
+import com.example.cheapjetshark.network.CheapSharkApi
+import com.example.cheapjetshark.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(Singleton::class)
 class AppModule {
+
+    //Retrofit
+    @Provides
+    @Singleton
+    fun providesCheapSharkApi(): CheapSharkApi = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(CheapSharkApi::class.java)
+
 }
