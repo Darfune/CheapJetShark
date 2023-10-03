353ffdb911144fec8de35e4f9e50c9b4bf5be771
Darfune <harolep@gmail.com>
2023-10-03 16:36:22 +0300

    AuthScreen setup

--- app/src/main/java/com/example/cheapjetshark/screens/auth/AuthScreen.kt
+++ app/src/main/java/com/example/cheapjetshark/screens/auth/AuthScreen.kt
@@ -1,21 +1,164 @@
 package com.example.cheapjetshark.screens.auth
 
+import android.widget.Space
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.cheapjetshark.navigation.root.NavigationGraph
 
+
 @Composable
 fun AuthScreen(navController: NavController) {
-    Text(text = "Auth Screen")
-    Button(
-        onClick = {
-            navController.popBackStack()
-            navController.navigate(NavigationGraph.HOME)
-        },
-    ) {
-        Text(text = "login")
-    }
 
+
+}
+
+@Preview
+@Composable
+fun LoginScreen() {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 20.dp, bottom = 20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopSection()
+            Spacer(modifier = Modifier.height(36.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 30.dp)
+            ) {
+                LoginTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    label = "Email",
+                    trailing = "",
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                LoginTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    label = "Password",
+                    trailing = "",
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = MaterialTheme.colorScheme.onPrimary,
+                        containerColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    shape = RoundedCornerShape(size = 4.dp)
+                ) {
+                    Text(text = "Log In")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun TopSection() {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = MaterialTheme.colorScheme.primary,
+                    fontSize = 50.sp,
+                    fontFamily = FontFamily.Default,
+                    fontWeight = FontWeight.Bold
+                )
+            ) {
+                append("C")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 42.sp,
+                    fontFamily = FontFamily.Default,
+                    fontWeight = FontWeight.Bold
+                )
+            ) {
+                append("heap")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontSize = 50.sp,
+                    fontFamily = FontFamily.Default,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic
+                )
+            ) {
+                append("JET")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = MaterialTheme.colorScheme.primary,
+                    fontSize = 50.sp,
+                    fontFamily = FontFamily.Default,
+                    fontWeight = FontWeight.Bold
+                )
+            ) {
+                append("S")
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 42.sp,
+                    fontFamily = FontFamily.Default,
+                    fontWeight = FontWeight.Bold
+                )
+            ) {
+                append("hark")
+            }
+        }
+    )
+    Row(
+        modifier = Modifier.padding(top = 80.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(
+                text = "Find the best prices",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
 }
