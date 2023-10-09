20fd1c12e81448d3596301630825dd028523a0f2
Darfune <harolep@gmail.com>
2023-10-09 16:52:16 +0300

    used can't go back to splashscreen

--- app/src/main/java/com/example/cheapjetshark/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/cheapjetshark/screens/splash/SplashScreen.kt
@@ -3,10 +3,15 @@ package com.example.cheapjetshark.screens.splash
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import com.example.cheapjetshark.navigation.start.AuthScreens
 
 @Composable
 fun SplashScreen(navController: NavController) {
     Text(text = "Splash Screen")
-    navController.navigate(AuthScreens.AuthScreen.name)
+    navController.navigate(AuthScreens.AuthScreen.name){
+        popUpTo(navController.graph.id){
+            inclusive = true
+        }
+    }
 }
