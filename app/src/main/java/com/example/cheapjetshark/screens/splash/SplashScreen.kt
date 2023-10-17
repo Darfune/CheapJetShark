0c675b851090f63cedab93067de77fac5029a8d9
Darfune <harolep@gmail.com>
2023-10-17 20:23:54 +0300

    create entry of user in Firebase database

--- app/src/main/java/com/example/cheapjetshark/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/cheapjetshark/screens/splash/SplashScreen.kt
@@ -1,9 +1,9 @@
 package com.example.cheapjetshark.screens.splash
 
+import android.util.Log
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import com.example.cheapjetshark.navigation.root.NavigationGraph
 import com.example.cheapjetshark.navigation.start.AuthScreens
 import com.google.firebase.auth.FirebaseAuth
@@ -13,7 +13,7 @@ fun SplashScreen(navController: NavController) {
     Text(text = "Splash Screen")
 
     //Todo: Check if there's a user, if so take them to Home Screen, otherwise, to AuthScreens
-    if (FirebaseAuth.getInstance().currentUser!!.email.isNullOrEmpty()){
+    if (FirebaseAuth.getInstance().currentUser?.email.isNullOrEmpty()){
         navController.navigate(AuthScreens.LogInScreen.name){
             popUpTo(navController.graph.id){
                 inclusive = true
