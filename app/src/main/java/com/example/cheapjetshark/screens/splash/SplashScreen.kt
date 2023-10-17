b97fbf36c86a97c9569328e91e27853b7aff605f
Darfune <harolep@gmail.com>
2023-10-17 19:49:48 +0300

    checks if a user is already logged in

--- app/src/main/java/com/example/cheapjetshark/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/cheapjetshark/screens/splash/SplashScreen.kt
@@ -4,14 +4,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import com.example.cheapjetshark.navigation.root.NavigationGraph
 import com.example.cheapjetshark.navigation.start.AuthScreens
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun SplashScreen(navController: NavController) {
     Text(text = "Splash Screen")
-    navController.navigate(AuthScreens.LogInScreen.name){
-        popUpTo(navController.graph.id){
-            inclusive = true
+
+    //Todo: Check if there's a user, if so take them to Home Screen, otherwise, to AuthScreens
+    if (FirebaseAuth.getInstance().currentUser!!.email.isNullOrEmpty()){
+        navController.navigate(AuthScreens.LogInScreen.name){
+            popUpTo(navController.graph.id){
+                inclusive = true
+            }
+        }
+    } else {
+        navController.navigate(NavigationGraph.HOME) {
+            popUpTo(NavigationGraph.AUTH) {
+                inclusive = true
+            }
         }
     }
+
+
 }
