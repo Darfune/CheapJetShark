4f937e216546c12a2fc6280c1839ce1f5b580221
Darfune <harolep@gmail.com>
2023-10-17 19:36:59 +0300

    can sign in and create account

--- app/src/main/java/com/example/cheapjetshark/screens/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/cheapjetshark/screens/auth/AuthViewModel.kt
@@ -1,4 +1,55 @@
 package com.example.cheapjetshark.screens.auth
 
-class AuthViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+
+class AuthViewModel : ViewModel() {
+    //    val loadingState = MutableStateFlow(LoadingState.IDLE)
+    private val auth: FirebaseAuth = Firebase.auth
+
+    private val _loading = MutableLiveData(false)
+    val loading: LiveData<Boolean> = _loading
+
+    fun signInWithEmailAndPass(email: String, password: String, home: () -> Unit) =
+        viewModelScope.launch {
+            try {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("Auth Success", "signInWithEmailAndPass: ${task.result}")
+                        //Todo: "take to HomeScreen"
+                        home()
+                    } else {
+                        Log.d("Auth Error", "signInWithEmailAndPass: ${task.result}")
+                    }
+                }
+            } catch (ex: Exception) {
+                Log.d("Auth Error", "signInWithEmailAndPass: ${ex.message}")
+            }
+        }
+
+    fun createUserWithEmailAndPass(
+        email: String,
+        password: String,
+        home: () -> Unit
+    ) {
+        if (_loading.value == false){
+            _loading.value = true
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        home()
+                    } else{
+                        Log.d("Auth Error", "createUserWithEmailAndPass: ${task.result}")
+                    }
+                    _loading.value = false
+                }
+        }
+    }
 }
