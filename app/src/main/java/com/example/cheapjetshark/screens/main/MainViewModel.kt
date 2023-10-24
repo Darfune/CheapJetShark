853f0c1748e1620824892bc636fde5c3c69c20e8
Darfune <harolep@gmail.com>
2023-10-24 17:22:30 +0300

    Can display 20 latest deals

--- app/src/main/java/com/example/cheapjetshark/screens/main/MainViewModel.kt
+++ app/src/main/java/com/example/cheapjetshark/screens/main/MainViewModel.kt
@@ -1,36 +1,48 @@
 package com.example.cheapjetshark.screens.main
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cheapjetshark.data.DataOrException
 import com.example.cheapjetshark.models.deals.DealsList
 import com.example.cheapjetshark.repository.ApiRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val apiRepository: ApiRepository) : ViewModel() {
-    val dealsList: MutableState<DataOrException<DealsList, Boolean, Exception>> = mutableStateOf(
+    private val _newest20DealsList: MutableLiveData<DataOrException<DealsList, Boolean, Exception>> = MutableLiveData(
         DataOrException(null, true, Exception(""))
     )
+    val newest20DealsList: LiveData<DataOrException<DealsList, Boolean, Exception>> = _newest20DealsList
 
-    suspend fun getListOfDeals(
+    init {
+        viewModelScope.launch { getListOfDeals(sortBy = "Recent", pageSize = 20) }
+    }
+
+    private suspend fun getListOfDeals(
         storeID: Int? = null,
         upperPrice: Int? = null,
         lowerPrice: Int? = null,
         sortBy: String? = null,
         pageNumber: Int? = null,
+        pageSize: Int? = null,
         title: String? = null
     ): DataOrException<DealsList, Boolean, Exception> {
-        return apiRepository.getListOfDeals(
+        _newest20DealsList.value = apiRepository.getListOfDeals(
             storeID = storeID,
             upperPrice = upperPrice,
             lowerPrice = lowerPrice,
             sortBy = sortBy,
             pageNumber = pageNumber,
+            pageSize = pageSize,
             title = title
         )
+        Log.d("MainViewModel", "getListOfDeals: ${_newest20DealsList.value!!.data!!.size}")
+        return _newest20DealsList.value!!
     }
 
 }
--- app/src/main/java/com/example/cheapjetshark/screens/main/home/HomeScreen.kt
+++ app/src/main/java/com/example/cheapjetshark/screens/main/home/HomeScreen.kt
@@ -1,57 +1,84 @@
 package com.example.cheapjetshark.screens.main.home
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import android.util.Log
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.rounded.Favorite
-import androidx.compose.material.icons.rounded.FavoriteBorder
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.example.cheapjetshark.R
+import com.example.cheapjetshark.data.DataOrException
+import com.example.cheapjetshark.models.deals.DealsList
 import com.example.cheapjetshark.models.deals.DealsListItem
 import com.example.cheapjetshark.screens.main.MainViewModel
-import com.example.cheapjetshark.ui.theme.notOnSaleColor
-import com.example.cheapjetshark.ui.theme.onSaleColor
+import com.example.cheapjetshark.screens.main.home.components.DealListItem
 
 @Composable
 fun HomeScreen(
     navController: NavController,
     viewModel: MainViewModel = hiltViewModel()
 ) {
+
     Surface(modifier = Modifier.fillMaxSize()) {
 
+
 //        DealListItem()
+        NewestDealsArea(navController, listOfDeals = viewModel.newest20DealsList.value?.data) {
+
+        }
     }
+}
+
+@Composable
+fun NewestDealsArea(
+    navController: NavController,
+    listOfDeals: DealsList?,
+    onDealPressed: (String) -> Unit
+) {
+    val scrollState = rememberScrollState()
+    Column {
+        TitleSection(label = "Newest Deals")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .horizontalScroll(scrollState)
+        ) {
+            if (!listOfDeals.isNullOrEmpty()) {
+                for (i in 0..<listOfDeals.size) {
+                    Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                        DealListItem(modifier = Modifier, deal = listOfDeals[i]) {
+                            onDealPressed(it)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TitleSection(label: String) {
+    Text(
+        text = label,
+        style = MaterialTheme.typography.titleLarge,
+        color = MaterialTheme.colorScheme.onBackground
+    )
 }
 
