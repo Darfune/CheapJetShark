17a5a2f535f4ed2d8659fd52a2ef406d2254ddf7
Darfune <harolep@gmail.com>
2023-10-24 17:22:15 +0300

    Details Screen setup

--- app/src/main/java/com/example/cheapjetshark/navigation/root/RootNavigation.kt
+++ app/src/main/java/com/example/cheapjetshark/navigation/root/RootNavigation.kt
@@ -1,15 +1,12 @@
 package com.example.cheapjetshark.navigation.root
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.cheapjetshark.navigation.start.authNavGraph
+import com.example.cheapjetshark.navigation.auth.authNavGraph
 import com.example.cheapjetshark.navigation.home.MainScreen
-import com.example.cheapjetshark.repository.ApiRepository
-import com.example.cheapjetshark.screens.main.MainViewModel
+import com.example.cheapjetshark.screens.derails.DetailsScreen
 import com.example.cheapjetshark.screens.splash.SplashScreen
 @Composable
 fun CheapJetSharkNavigation(navController: NavHostController) {
@@ -25,6 +22,9 @@ fun CheapJetSharkNavigation(navController: NavHostController) {
         composable(route = NavigationGraph.HOME) {
             MainScreen(rootNavController = navController)
         }
+        composable(route = NavigationGraph.DETAILS){
+            DetailsScreen(navController = navController)
+        }
 
     }
 }
@@ -34,5 +34,6 @@ object NavigationGraph {
     const val SPLASH = "splash_graph"
     const val AUTH = "auth_graph"
     const val HOME = "home_graph"
+    const val DETAILS = "details_graph"
 }
 
