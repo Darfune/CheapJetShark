993e1b18c87a04b6c55a485cf7d4bf4bcf196062
Darfune <harolep@gmail.com>
2023-10-24 17:21:40 +0300

    Details Screen setup

---
+++ app/src/main/java/com/example/cheapjetshark/screens/derails/DetailsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.cheapjetshark.screens.derails
+
+import android.telecom.Call.Details
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun DetailsScreen(navController: NavHostController) {
+    Text(text = "Details Screen")
+}
