42894bc8db62d16ec0103f6041e88855942b9391
Darfune <harolep@gmail.com>
2023-10-27 19:04:46 +0300

    search setup

--- app/src/main/java/com/example/cheapjetshark/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/cheapjetshark/screens/search/SearchScreen.kt
@@ -1,21 +1,16 @@
 package com.example.cheapjetshark.screens.search
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -23,18 +18,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.example.cheapjetshark.R
-import com.example.cheapjetshark.navigation.root.NavigationGraph
+import androidx.navigation.compose.rememberNavController
+import com.example.cheapjetshark.models.gamesbysearch.GamesBySearch
+import com.example.cheapjetshark.screens.search.components.BackIconButton
+import com.example.cheapjetshark.screens.search.components.GamesSearchBar
+import com.example.cheapjetshark.screens.search.components.SearchGameRow
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun SearchScreen(navController: NavHostController) {
+fun SearchScreen(
+    navController: NavHostController,
+    viewModel: SearchViewModel = hiltViewModel()
+) {
 
     val searchItem = rememberSaveable {
         mutableStateOf("")
@@ -42,56 +41,38 @@ fun SearchScreen(navController: NavHostController) {
     val keyboardController = LocalSoftwareKeyboardController.current
 
     Surface(modifier = Modifier.fillMaxSize()) {
-        Column {
+        Column(verticalArrangement = Arrangement.Top) {
             Row(
                 modifier = Modifier.padding(start = 8.dp, top = 16.dp),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                IconButton(
-                    onClick = {
-                        navController.navigate(NavigationGraph.HOME) {
-                            popUpTo(NavigationGraph.SEARCH){
-                                inclusive = true
-                            }
-                        }
-                    },
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_back_arrow),
-                        contentDescription = "Exit search screen"
-                    )
+                BackIconButton(navController)
+                GamesSearchBar(searchItem) {
+                    keyboardController?.hide()
+                    viewModel.getSearchedTitleList(searchItem.value)
                 }
-                OutlinedTextField(
-                    value = searchItem.value,
-                    onValueChange = { searchItem.value = it },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 16.dp, end = 16.dp),
-                    placeholder = {
-                        Text(
-                            text = "Search",
-                            style = MaterialTheme.typography.labelMedium,
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Text,
-                        imeAction = ImeAction.Search
-                    ),
-                    keyboardActions = KeyboardActions(
-                        onSearch = {
-                            if (searchItem.value.isNotEmpty()){
-                                keyboardController?.hide()
-                                Log.d("Searched", "SearchScreen: ${searchItem.value}")
-                            }
-                        }
-                    )
-
-                )
             }
+            Spacer(modifier = Modifier.height(16.dp))
+            GamesList(navController, viewModel.searchTitle.value?.data)
         }
 
     }
 
 }
+
+@Composable
+fun GamesList(navController: NavHostController, gameList: GamesBySearch?) {
+
+    LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(4.dp)) {
+
+//        items(items = gameList) { game ->
+//            SearchGameRow(game) {
+//                // TODO: navigate to the details screen
+//            }
+//        }
+
+    }
+}
+
+
