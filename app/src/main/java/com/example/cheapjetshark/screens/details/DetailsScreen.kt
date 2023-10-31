83d00695f322a121790cbb09c845d67d6a990048
Darfune <harolep@gmail.com>
2023-10-31 16:07:13 +0200

    details screen ui setup

--- app/src/main/java/com/example/cheapjetshark/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/cheapjetshark/screens/details/DetailsScreen.kt
@@ -1,10 +1,138 @@
 package com.example.cheapjetshark.screens.details
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.example.cheapjetshark.R
+import com.example.cheapjetshark.screens.details.components.DealsRow
+import com.example.cheapjetshark.ui.theme.onSaleColor
 
+@Preview
 @Composable
-fun DetailsScreen(navController: NavHostController) {
-    Text(text = "Details Screen")
+fun DetailsScreen(navController: NavHostController = rememberNavController()) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxHeight(.3f)
+                    .fillMaxWidth()
+            ) {
+                AsyncImage(
+                    model = "https://cdn.fanatical.com/production/product/400x225/105f34ca-7757-47ad-953e-7df7f016741e.jpeg",
+                    placeholder = painterResource(R.drawable.ic_temp_game_placeholder),
+                    contentDescription = "Game Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            Text(
+                text = "Title of Game: More text for the Game",
+                style = MaterialTheme.typography.titleLarge,
+                softWrap = true,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(8.dp)
+            )
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+                    ) {
+                        append("Lowest recorded price: ")
+                    }
+                    withStyle(
+                        style = SpanStyle(
+                            color = onSaleColor
+                        )
+                    ) {
+                        append("3.99$")
+                    }
+                },
+                style = MaterialTheme.typography.titleSmall,
+                softWrap = true,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(start = 8.dp, top = 16.dp)
+            )
+            Surface(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(top = 8.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 16.dp,
+                            topEnd = 16.dp
+                        )
+                    ), tonalElevation = 1.dp
+            ) {
+                if (false) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(100.dp)
+                        )
+                    }
+
+                } else {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                        contentPadding = PaddingValues(4.dp)
+                    ) {
+                        items(count = 6) { deal ->
+                            DealsRow()
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
+
---
+++ app/src/main/java/com/example/cheapjetshark/screens/details/components/DealsRow.kt
@@ -0,0 +1,107 @@
+package com.example.cheapjetshark.screens.details.components
+
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.cheapjetshark.R
+import com.example.cheapjetshark.models.gamesbysearch.GamesBySearchItem
+import com.example.cheapjetshark.ui.theme.onSaleColor
+import kotlin.math.floor
+
+@Preview
+@Composable
+fun DealsRow() {
+
+    Surface(
+        modifier = Modifier
+            .clickable
+            { }
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(8.dp),
+        tonalElevation = 3.dp,
+        shape = RoundedCornerShape(8.dp)
+    )
+    {
+        Row(modifier = Modifier.padding(6.dp), verticalAlignment = Alignment.Top) {
+
+            Surface(tonalElevation = 6.dp, shape = RoundedCornerShape(8.dp)) {
+                AsyncImage(
+                    model = "https://www.cheapshark.com/img/stores/logos/0.png",
+                    placeholder = painterResource(R.drawable.ic_temp_game_placeholder),
+                    contentDescription = "Game Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth(0.3f)
+                        .fillMaxHeight()
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(start = 16.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                Text(
+                    text = "Steam",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Row(modifier = Modifier.padding(end = 8.dp)) {
+                    Text(
+                        text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = MaterialTheme.colorScheme.onBackground
+                                )
+                            ) {
+                                append("19.99 → 4.99 - Down: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = onSaleColor
+                                )
+                            ) {
+                                append(floor(78.839420).toString() + "%")
+                            }
+
+                        },
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            }
+        }
+    }
+
+
+}
